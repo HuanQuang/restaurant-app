a9696830ca3e55199f976852fb85d95b44096fa4
Trần Quang Huấn <hyugalaputa@gmail.com>
2022-09-29 16:33:32 +0700

    first commit

--- src/firebase-conflig.js
+++ src/firebase-conflig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
     apiKey: 'AIzaSyC2oqUnZ8S-zibbVRJ_38D9z1E9GMfEhSI',
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    authDomain: 'food-app-aa205.firebaseapp.com',
+    projectId: 'food-app-aa205',
+    storageBucket: 'food-app-aa205.appspot.com',
     messagingSenderId: '317292877601',
     appId: '1:317292877601:web:fdb92de5b743212f135060',
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+    measurementId: 'G-HWNVCKC3PK',
 };
 
 const app = initializeApp(firebaseConfig);
